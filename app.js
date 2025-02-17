e2af960630b353f528123e22cc2b30685326ffa3
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2025-02-17 09:51:17 +0500

    .

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+
+// ___________________________________________________________
+// let countDownDate = new Date("February 28, 2025 00:00:00").getTime();
+// let x =(function(){
+//     let now = new Date().getTime();
+//     let distance = countDownDate - now;
+//     let days = Math.floor(distance % (1000 * 60 * 60 * 24)) / (1000* 60 *60);
+//     let hours = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+//     document.getElementById("days").innerHTML = days;
+//         document.getElementById("hours").innerHTML = hours;
+//         document.getElementById("minutes").innerHTML = minutes; 
+//         document.getElementById("seconds").innerHTML = seconds;
+// })
 let countDownDate = new Date("February 28, 2025 00:00:00").getTime();
     let x = setInterval(function(){
     let now = new Date().getTime();
     let distance = countDownDate - now;
-
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
     document.getElementById("days").innerHTML = days;
-document.getElementById("hours").innerHTML = hours;
-document.getElementById("minutes").innerHTML = minutes; 
-document.getElementById("seconds").innerHTML = seconds;
-    });
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes; 
+    document.getElementById("seconds").innerHTML = seconds;
+});
